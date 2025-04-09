a9ce7faf931c875d410fd7280db0b2c2a146a217
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-04-09 17:07:20 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Popcorn Roleplay, Francis of NoVa mods, FiveM Community'
+description 'An open-source menu for using American Sign Language in FiveM'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+server_script {
+    'server.lua',
+}
