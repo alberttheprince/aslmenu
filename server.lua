0bc5ad1a8f81d8d503f1fab8f15b9aded7e20b3f
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-08-21 15:24:47 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,15 +4,15 @@ AddEventHandler('asl:syncAnimation', function(targetPlayerId, animationData)
     local source = source
     
     -- Verify players are near each other
-    local sourceCoords = GetEntityCoords(GetPlayerPed(source)) 
-    local targetCoords = GetEntityCoords(GetPlayerPed(targetPlayerId)) 
+    local sourceCoords = GetEntityCoords(GetPlayerPed(source))
+    local targetCoords = GetEntityCoords(GetPlayerPed(targetPlayerId))
     local distance = #(sourceCoords - targetCoords)
     
     if distance <= 15.0 then
         -- Notify target player that someone is signing to them
         TriggerClientEvent('asl:receiveAnimation', targetPlayerId, source, animationData)
     end
-end) 
+end)
 
 -- Log ASL usage (optional)
 RegisterNetEvent('asl:logUsage')
