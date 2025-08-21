64c0fdf505a754611f0c3c9ee2cb541a4731082b
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-08-21 14:17:32 -0400

    add config

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,17 @@ author 'Popcorn Roleplay, Francis of NoVa mods, FiveM Community'
 description 'An open-source menu for using American Sign Language in FiveM'
 version '1.0.0'
 
+shared_scripts {
+    'config.lua',
+}
+
 client_scripts {
     'client.lua',
 }
+
 server_script {
     'server.lua',
 }
+
+
+lua54 'yes'
